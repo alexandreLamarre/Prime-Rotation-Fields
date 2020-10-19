cfce9d12d277105316275ec363a944cf62406331
alexandreLamarre <alex7285@gmail.com>
2020-10-19 15:41:58 -0400

    Updates

---
+++ precache-manifest.47fb91922448430c93e95a891139a2b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53571a904fbcd2b4d113e7ebfaffaa57",
+    "url": "/Prime-Rotation-Fields/index.html"
+  },
+  {
+    "revision": "0f81900683a10aa259a1",
+    "url": "/Prime-Rotation-Fields/static/css/main.a7ee3ebd.chunk.css"
+  },
+  {
+    "revision": "089fd49a8d55811dec32",
+    "url": "/Prime-Rotation-Fields/static/js/2.c99167b5.chunk.js"
+  },
+  {
+    "revision": "939ef51a31f5fcdda7346e7a7f5f791d",
+    "url": "/Prime-Rotation-Fields/static/js/2.c99167b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f81900683a10aa259a1",
+    "url": "/Prime-Rotation-Fields/static/js/main.5904525c.chunk.js"
+  },
+  {
+    "revision": "8379b01d5a5ba461ec92",
+    "url": "/Prime-Rotation-Fields/static/js/runtime-main.c4f92673.js"
+  }
+]);
